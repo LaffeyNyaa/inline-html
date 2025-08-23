666ad292963d57188e251daeb99d23e31e64c600
LaffeyNyaa <laffeynyaa@outlook.com>
2025-08-23 09:59:00 +0800

    build: remove vars.cmake

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,8 +1,6 @@
 macro(add_example_target EXAMPLE_NAME SRCS)
   add_executable("${EXAMPLE_NAME}" "${SRCS}")
   set_target_properties("${EXAMPLE_NAME}" PROPERTIES CXX_STANDARD 20)
-  target_include_directories("${EXAMPLE_NAME}" PRIVATE "${INLINE_HTML_INCLUDE_DIR}"
-                                             include)
   target_link_libraries("${EXAMPLE_NAME}" PRIVATE "${PROJECT_NAME}")
   target_compile_features("${EXAMPLE_NAME}" PRIVATE cxx_std_20)
 
