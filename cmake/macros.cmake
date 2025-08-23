4cd81c4f5f5dd0cd68c8765301e5e62314526fff
LaffeyNyaa <laffeynyaa@outlook.com>
2025-08-23 19:05:17 +0800

    build: add double quotes

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,6 +1,5 @@
 macro(add_example_target EXAMPLE_NAME SRCS)
   add_executable("${EXAMPLE_NAME}" "${SRCS}")
-  set_target_properties("${EXAMPLE_NAME}" PROPERTIES CXX_STANDARD 20)
   target_link_libraries("${EXAMPLE_NAME}" PRIVATE "${PROJECT_NAME}")
   target_compile_features("${EXAMPLE_NAME}" PRIVATE "cxx_std_20")
 
